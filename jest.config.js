f1ad5e0727e29f567d410480955a671cb4f5f576
Muawia Asif <muawiaasif772@gmail.com>
2025-09-18 11:21:55 +0500

    Fix: React is not defined error during build

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  module.exports = {
+  
   presets: [
     '@babel/preset-env',
      "@babel/preset-react",// Enables JSX
        "@babel/preset-typescript"
 
   ],
-},
+
 transform: {
   "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { configFile: "./babel-jest.config.js" }]
 }
